1e62bf064dfc1a35177161c15c3555a881e88367
eDickC <cerevo@gmail.com>
2016-01-25 21:11:23 +0100

    tag-similarity algorithm added

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^basic', views.basic, name='basic'),    
     url(r'^user_view', views.user_view, name='user_view'),
     url(r'^rate_food', views.rate_food, name='rate_food'),
+    url(r'^show_all_foods', views.show_all_foods, name='show_all_foods'),
     url(r'^logout_view', views.logout_view), 
 ]
 
